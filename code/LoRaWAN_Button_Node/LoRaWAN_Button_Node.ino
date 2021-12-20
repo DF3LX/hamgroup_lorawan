5cd0d056bfa48fd4b7c601101172a9b06b1ddd5b
Felix, DO6FP <do6fp@darc.de>
2021-12-20 20:06:12 +0100

    fix

--- code/LoRaWAN_Button_Node/LoRaWAN_Button_Node.ino
+++ code/LoRaWAN_Button_Node/LoRaWAN_Button_Node.ino
@@ -27,18 +27,18 @@ SSD1306Wire display(0x3c, SDA, SCL);                // Display-Anschlüsse sind 
 // first. When copying an EUI from ttnctl output, this means to reverse
 // the bytes. For TTN issued EUIs the last bytes should be 0xD5, 0xB3,
 // 0x70.
-static const u1_t PROGMEM APPEUI[8]={ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+static const u1_t PROGMEM APPEUI[8]={  };
 void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
 
 // This should also be in little endian format, see above.
-static const u1_t PROGMEM DEVEUI[8]={ 0xBA, 0xA2, 0x04, 0xD0, 0x7E, 0xD5, 0xB3, 0x70 };
+static const u1_t PROGMEM DEVEUI[8]={  };
 void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
 
 // This key should be in big endian format (or, since it is not really a
 // number but a block of memory, endianness does not really apply). In
 // practice, a key taken from ttnctl can be copied as-is.
 // The key shown here is the semtech default key.
-static const u1_t PROGMEM APPKEY[16] = { 0x39, 0x45, 0x18, 0xE5, 0x2D, 0x9E, 0x77, 0x4B, 0x50, 0xB8, 0x88, 0x12, 0xE9, 0xAD, 0x85, 0x05 };
+static const u1_t PROGMEM APPKEY[16] = {  };
 void os_getDevKey (u1_t* buf) {  memcpy_P(buf, APPKEY, 16);}
 
 
