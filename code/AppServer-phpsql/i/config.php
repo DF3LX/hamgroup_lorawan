dc11386954ebade1273fd0b43e692326745c765d
JM <pp1@jmayer.de>
2022-01-30 21:05:32 +0100

    config.php ohne Zugangsdaten hinzgefügt

---
+++ code/AppServer-phpsql/i/config.php
@@ -0,0 +1,11 @@
+<?
+
+$dbZugangsdaten = array(
+    "host" => "localhost",
+    "port" => 3306,
+    "user" => "=== eintragen ===",
+    "pass" => "=== eintragen ===",
+    "name" => "DarcHamGroupLoRaWAN",
+);
+
+?>
